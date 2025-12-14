8e2936814d8bfae97b82239e57f377c87b6dfed2
Zach McNulty <72584137+zach-mcnulty@users.noreply.github.com>
2025-12-14 23:24:28 +0000

    updating viteconfig

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss(),],
-  base: '/typeracer/'
+  base: '/typeracer/',
+  assetsInclude: ['**/gauge.js']
 })
