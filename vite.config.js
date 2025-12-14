e6d2138f1847d631500dc18b4cf60e6b0da3e85b
Zach McNulty <72584137+zach-mcnulty@users.noreply.github.com>
2025-12-14 23:28:59 +0000

    updating gauge location and viteconfig

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss(),],
-  base: '/typeracer/',
-  assetsInclude: ['**/gauge.js']
+  base: '/typeracer/'
 })
